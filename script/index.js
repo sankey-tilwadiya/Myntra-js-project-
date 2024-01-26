7220cc020d022676e487da02fdac73f4cadcba94
sankey-tilwadiya <ssankeygautam@gmail.com>
2024-01-26 14:50:36 +0530

    Add js

---
+++ script/index.js
@@ -0,0 +1,69 @@
+// let itemsContainerElement =document.querySelectorAll('.items-container');
+// itemsContainerElement.innerHTML=`<div className="item-container">
+// <img className="item-image" src="images/1 (1).jpg" alt="item image"/> 
+// <div className="rating">4.5⭐| 1.4k
+// </div>
+// <div className="company-name"></div>
+// <div class="item-name"><div>
+// <div className="price">
+// <span className="current-price">Rs 600</span>
+// <span className="discount-price">(42% OFF)</span>
+// <span className="orignal-price">Rs 1045</span>
+// </div>
+
+// <button  className="bag-btn">Add to Bag</button>
+// </div>`;
+let bagItems;
+onLoad();
+
+function onLoad() {
+  let bagItemsStr = localStorage.getItem('bagItems');
+  bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+}
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    console.log('I am here');
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerText = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  }
+}
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector('.items-container');
+  if (!itemsContainerElement) {
+    return;
+  }
+  let innerHtml = '';
+  items.forEach(item => {
+    innerHtml += `
+    <div class="item-container">
+      <img class="item-image" src="${item.image}" alt="item image">
+      <div class="rating">
+          ${item.rating.stars} ⭐ | ${item.rating.count}
+      </div>
+      <div class="company-name">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
+      <div class="price">
+          <span class="current-price">Rs ${item.current_price}</span>
+          <span class="original-price">Rs ${item.original_price}</span>
+          <span class="discount">(${item.discount_percentage}% OFF)</span>
+      </div>
+      <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+    </div>`
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
+
+
