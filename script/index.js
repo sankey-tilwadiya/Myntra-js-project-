743cf912a5569a26523e2e884f948c16e61d2b14
sankey-tilwadiya <ssankeygautam@gmail.com>
2024-02-13 13:04:32 +0530

    Add pont

--- script/index.js
+++ script/index.js
@@ -31,13 +31,7 @@ function addToBag(itemId) {
 
 function displayBagIcon() {
   let bagItemCountElement = document.querySelector('.bag-item-count');
-  if (bagItems.length > 0) {
-    console.log('I am here');
-    bagItemCountElement.style.visibility = 'visible';
-    bagItemCountElement.innerText = bagItems.length;
-  } else {
-    bagItemCountElement.style.visibility = 'hidden';
-  }
+
 }
 
 function displayItemsOnHomePage() {
